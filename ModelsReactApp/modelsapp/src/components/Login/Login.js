2835d0dd63262a2656b94f2ce7fab7007e7bcc31
Alexander Thougaard Domino <202108571@post.au.dk>
2023-05-10 12:58:45 +0200

    implemented the first Login Hook

---
+++ ModelsReactApp/modelsapp/src/components/Login/Login.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { login } from '../utils/api';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await login(username, password);
+            const { token } = response.data;
+
+            // Save the token in local storage
+            localStorage.setItem('token', token);
+
+            // Redirect to the dashboard
+            window.location = '/dashboard';
+        } catch (error) {
+            setError(error.response.data.message);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+            </label>
+            <button type="submit">Login</button>
+            {error && <div>{error}</div>}
+        </form>
+    );
+}
+
+export default Login;
