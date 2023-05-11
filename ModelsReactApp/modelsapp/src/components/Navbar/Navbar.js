f1d91f50329d2d9a3083a0936cc2e9c291a7c48d
Alexander Thougaard Domino <202108571@post.au.dk>
2023-05-11 13:51:42 +0200

    made the Navbar beautiful in Navbar.css

--- ModelsReactApp/modelsapp/src/components/Navbar/Navbar.js
+++ ModelsReactApp/modelsapp/src/components/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 import { NavLink } from "react-router-dom";
+import './Navbar.css';
 
 export function Navbar() {
     return (
         <nav>
-            <NavLink to="/Login" >
-                Log out
-            </NavLink>
             <NavLink to="/Manager" >
                 Manager page
             </NavLink>
-
+            <NavLink to="/Login" >
+                Log out
+            </NavLink>
         </nav>
     );
 }
