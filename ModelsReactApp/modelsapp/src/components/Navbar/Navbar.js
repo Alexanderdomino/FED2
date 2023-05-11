5e8607d0d3746102b90845a4a0b3e2a1f5309cda
Alexander Thougaard Domino <202108571@post.au.dk>
2023-05-11 13:56:38 +0200

    added home to navBar

--- ModelsReactApp/modelsapp/src/components/Navbar/Navbar.js
+++ ModelsReactApp/modelsapp/src/components/Navbar/Navbar.js
@@ -4,6 +4,9 @@ import './Navbar.css';
 export function Navbar() {
     return (
         <nav>
+            <NavLink to="/" >
+                Home
+            </NavLink>
             <NavLink to="/Manager" >
                 Manager page
             </NavLink>
